d5a2040aad7e5e28bf57a166f122610ec96ef3bb
escaaaaaa <hannmha@stud.ntnu.no>
2025-02-21 20:14:45 +0100

    Update test_HandOfCards.py

--- Enhetstesting/test_HandOfCards.py
+++ Enhetstesting/test_HandOfCards.py
@@ -4,5 +4,5 @@ import sys, os
 # Add the parent directory of 'notebooks' to the sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), 'src')))
 
-from DeckOfCards import DeckOfCards
-from HandOfCards import HandOfCards
+from src.DeckOfCards import DeckOfCards
+from src.HandOfCards import HandOfCards
