2c3210c1f60f45e5c8c8016bec52aa7368c53db2
escaaaaaa <hannmha@stud.ntnu.no>
2025-02-21 19:49:48 +0100

    Create test_HandOfCards.py

---
+++ Enhetstesting/test_HandOfCards.py
@@ -0,0 +1,8 @@
+import unittest
+import sys, os
+
+# Add the parent directory of 'notebooks' to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), 'src')))
+
+from DeckOfCards import DeckOfCards
+from HandOfCards import HandOfCards
