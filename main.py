551c3318c6ade574c3e5bd1666352dad1892deec
escaaaaaa <hannmha@stud.ntnu.no>
2025-02-22 21:29:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from DeckOfCards import DeckOfCards
+import pickle
 
 Deck=DeckOfCards()
 Check=0
+confirm_exit=False
 
 print('')
 print('Welcome to this cardgame!')
@@ -12,16 +14,31 @@ print('Good luck!')
 print('')
 
 
+#Function to save a hand of cards to a file for later use
+def save_hand(hand, filename):
+    #Arguments:
+        #hand: TenHadOfCards object to be saved
+        #filmename (str): The name of the save file
+    with open(filename, 'wb') as file:
+        pickle.dump(hand, file)
+    print(f"Hand saved to {filename}")
+
 def menu():   
     global Check 
-    global Cards
+    global confirm_exit
     while True:
+        if confirm_exit==True:
+            break
+        else:
+            pass
         print('')
         print('Choose an option from the menu by entering a number:')
         print('1: Deal a new hand of cards')
         print('2: See the cards I currently have')
         print('3: Check for a flush')
-        print('4: Exit the game')
+        print('4: Save you hand of cards to a file')
+        print('5: Upload a hand of cards from a file')
+        print('6: Exit the game')
         x=input('I want to: ')
         print('')
         # User input for dealing a hand of cards
@@ -57,19 +74,34 @@ def menu():
                     print("Gasp! You have the Queen of spades!")
                 else:
                     print('No Queen of spades for you!')
-        #User input for ending the function
+ 
+ 
         elif x == '4':
-            print('Thank you for playing my card game!')
-            print('')
-            break
+            pass
+ 
+ 
+        elif x == '5':
+            pass
+
+
+        #User input for ending the function
+        elif x == '6':
+            while True:
+                answer=input("Are you sure? (yes/no)")
+                if answer.lower() == "yes":
+                    print('')
+                    print('Thank you for playing my card game!')
+                    print('')
+                    confirm_exit=True
+                    break
+                elif answer == "no":
+                    break
+                else:
+                    print('That is not a valid option, please type yes or no!')
         else:
             print('That is not a valid option, please choose again:')
 
 menu()
 
-answer=input("Are you sure?")
-if answer.lower == "yes":
-    pass
-else:
-    menu()
+
 
